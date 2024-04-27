900a49a39925e315cad38425d75de466c1f02962
Ayberkyvs <ayberkyavas9@gmail.com>
2024-04-27 18:59:42 +0000

    Build: (f544f5e) Update node.js.yml

--- future.md
+++
@@ -1,8 +0,0 @@
-# Refactor the component names
-# Refactor the codes for less space
-# Write Project Detail datas
-# Make fancy framer motion text area like remixjs website
-# Find solution for cards, separate the blog link and certificate link for better ux
-# Refactor the sitemap.xml for google indexing
-# Increase the accessablity, seo, performance
-# add at least one minigame for fun idk, maybe i want to show my power!!!!!!!!!..
--- src/components/BackgroundGradient.tsx
+++
@@ -1,8 +0,0 @@
-import { motion } from "framer-motion";
-
-export default function BackgroundGradient({variant}: {variant: string}) {
-    return (
-        <motion.div className={`${variant === "header" ? "gradient-top" : "gradient-bottom"} w-full md:max-w-[1440px] h-[659px] overflow-hidden`} draggable={false} initial={{opacity: 0 }} whileInView={{opacity: 1}} transition={{duration: 0.5}} aria-disabled="true"></motion.div>
-    )
-}
-
