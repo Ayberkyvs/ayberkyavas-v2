a9e43e249dde7b2e09f7b6865e5297fe357fc0f4
Ayberk Yavas <ayberkyavas9@gmail.com>
2024-05-17 17:37:22 +0300

    update

---
+++ src/components/ReferenceCard.tsx
@@ -0,0 +1,25 @@
+import { Link as RouterLink} from "react-router-dom"
+import BadgeElement from "./BadgeElement"
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import Socials from "./Header/Socials";
+import { BadgeCheck } from "lucide-react";
+
+export default function ReferenceCards({ data, isResponsive = false }: { data: any, isResponsive?: boolean}) {
+  return (
+    <div className={`flex items-center rounded-[16px] bg-transparent hover:bg-gradient-to-r from-[rgba(255,233,244,0.4)] to-[rgba(234,243,255,0.6)]
+    dark:from-[#1E1C32]/50 dark:to-[#2B161D]/50 h-fit p-[12px]
+     text-primary-text cursor-pointer w-[224px] flex-col md:min-h-[302px]`}>
+       <div className={`relative ${isResponsive ? "w-[100px] h-[100px] min-w-[100px] min-h-[100px] mr-[12px] md:mr-0" : "w-[200px] h-[200px] min-w-[200px] min-h-[200px]"} rounded-xl md:w-[200px] md:h-[200px] mb-[8px]`}>
+         <BadgeElement className={`absolute top-[8px] left-[8px] ${data.badgeText ? "flex": "hidden"} justify-center items-center w-fit h-[16px] px-[6px] py-[0px] text-[8px] md:text-[10px] uppercase text-white`} variant={data.badgeType}>{data.badgeText}</BadgeElement>
+         <LazyLoadImage src={data.image} alt={data.company + " logo"} className="w-full h-full rounded-xl object-cover" draggable={false} />
+       </div>
+       <div className={`flex flex-col w-fit h-fit`}>
+           <h3 className="inline-flex justify-start items-center text-base font-bold mr-2">{data.title} <BadgeCheck className="ml-1 inline text-link-blue w-[18px] h-[18px]"/></h3>
+           <p className="text-[13px] text-light-gray leading-[1.5em]">{data.company}</p>
+           <p className="text-sm text-primary-text-60 leading-[1.5em]">{data.paragraph}</p>
+           <Socials className="flex gap-3 mt-3" data={data.socials}/>
+       </div>
+   </div>
+  )
+}
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { DataContext } from "@/App"
 import { lazy, useContext } from "react"
 import { Pin } from "iconoir-react";
+import ReferenceCards from "@/components/ReferenceCard";
 const Hero = lazy(()=> import("@/components/Hero/Hero"));
 const ProjectCards = lazy(()=> import("@/components/ProjectCards"));
 const HorizontalScrollCards = lazy(()=> import("@/components/HorizontalScrollCards"));
@@ -10,11 +11,13 @@ export default function Home() {
   const data = useContext(DataContext).en
   return (
     <section className="w-full h-fit pt-[136px] md:pt-[126px]" >
-        <Hero data={data.hero}/>
-        <ProjectCards data={data.featured} icon={<Pin />}/>
-        <ProjectCards data={data.certificates}/>
-        <HorizontalScrollCards data={data.works} />
-        <Sayhi />
+      <Hero data={data.hero}/>
+      <ProjectCards data={data.featured} icon={<Pin />}/>
+      <ProjectCards data={data.certificates}/>
+      <ProjectCards data={data.works} />
+      <HorizontalScrollCards data={data.references} />
+      <Sayhi />
     </section>
   )
 }
+
