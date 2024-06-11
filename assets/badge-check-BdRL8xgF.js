e2501a8a637508daff66334019cf217546433230
Ayberkyvs <ayberkyavas9@gmail.com>
2024-06-11 23:02:55 +0000

    Build: (22dccef) Merge pull request #7 from Ayberkyvs/feature/new_icons
    All icons renewed

---
+++ assets/badge-check-BdRL8xgF.js
@@ -0,0 +1,6 @@
+import{c as e}from"./index-CH-6MX8j.js";/**
+ * @license lucide-react v0.358.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("BadgeCheck",[["path",{d:"M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z",key:"3c2336"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);export{c as B};
